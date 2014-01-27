458fcc579c389c51854ab745b36e294ac95d8d18
Alberto Pires <mrpenguin2005@gmail.com>
2014-01-27 15:43:44 -0200

    First commit/push

---
+++ camel/chapter1-file-copy/src/main/java/camelinaction/App.java
@@ -0,0 +1,13 @@
+package camelinaction;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
