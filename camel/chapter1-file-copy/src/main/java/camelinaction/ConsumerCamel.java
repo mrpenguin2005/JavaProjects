f6fa32d3c18ec8c08d3bab4b4048368a87a4ae6f
Alberto Pires <mrpenguin2005@gmail.com>
2014-02-03 17:03:48 -0200

    Camel Consumer

---
+++ camel/chapter1-file-copy/src/main/java/camelinaction/ConsumerCamel.java
@@ -0,0 +1,36 @@
+package camelinaction;
+
+import javax.jms.ConnectionFactory;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.camel.CamelContext;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.jms.JmsComponent;
+import org.apache.camel.impl.DefaultCamelContext;
+
+public class ConsumerCamel {
+	
+	public static void main(String args[]) throws Exception {
+		// create CamelContext
+		CamelContext context = new DefaultCamelContext();
+		ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://192.168.40.10:61616");
+
+		context.addComponent("jms", JmsComponent.jmsComponentAutoAcknowledge(connectionFactory));
+
+		// add our route to the CamelContext
+		context.addRoutes(new RouteBuilder() {
+			public void configure() {
+				from("jms:queue:incomingOrders").to("file:data/download");
+			}
+		});
+
+		// start the route and let it do its work
+		context.start();
+		//Thread.sleep(10000);
+		Thread.sleep(180000);
+
+		// stop the CamelContext
+		context.stop();
+	}
+
+}
