ed641f85b3df0ca25a65fd125794161fdda112b0
Alberto Pires <mrpenguin2005@gmail.com>
2014-02-18 15:05:21 -0300

    Configs/Shutdown hook

--- camel/chapter1-file-copy/src/main/java/camelinaction/ConsumerCamel.java
+++ camel/chapter1-file-copy/src/main/java/camelinaction/ConsumerCamel.java
@@ -1,5 +1,10 @@
 package camelinaction;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 import javax.jms.ConnectionFactory;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
@@ -11,26 +16,87 @@ import org.apache.camel.impl.DefaultCamelContext;
 public class ConsumerCamel {
 
 	public static void main(String args[]) throws Exception {
+		// Read Configuration
+		//Properties prop = loadConfig("c:\\tmp\\Imagens\\solicitacoes.alunos.properties");
+		String propFile = null;
+		if (args.length == 1) {
+			propFile = args[0];
+		}
+		Properties prop = loadConfig(propFile);
+		final String brokerUrl = prop.getProperty("broker.url");
+		final String brokerJmsQueue = prop.getProperty("broker.from.jms");
+		final String brokerFileQueue = prop.getProperty("broker.to.path");
+
+		System.out.println("Config File : "+propFile);
+		System.out.println("Broker URL  : "+brokerUrl);
+		System.out.println("Broker JMS  : "+brokerJmsQueue);
+		System.out.println("Broker File : "+brokerFileQueue);
 		// create CamelContext
-		CamelContext context = new DefaultCamelContext();
-		ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://192.168.40.10:61616");
+		final CamelContext context = new DefaultCamelContext();
+		//ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://192.168.40.10:61616");
+		ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(brokerUrl);
 
 		context.addComponent("jms", JmsComponent.jmsComponentAutoAcknowledge(connectionFactory));
 
 		// add our route to the CamelContext
 		context.addRoutes(new RouteBuilder() {
 			public void configure() {
-				from("jms:queue:solicitacoes").to("file:/var/www/ooo/solicitacoes");
+				//from("jms:queue:solicitacoes").to("file:/var/www/ooo/solicitacoes");
+				from(brokerJmsQueue).to(brokerFileQueue);
 			}
 		});
 
 		// start the route and let it do its work
 		context.start();
-		// Thread.sleep(10000);
-		Thread.sleep(3600000);
+		
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+			public void run() {
+				try {
+					context.stop();
+					waitSeconds(1);
+				} catch (Exception e) {
+					throw new RuntimeException(e);
+				}
+			}
+		});
 
 		// stop the CamelContext
-		context.stop();
+		//context.stop();
+	}
+	
+	public static void waitForever() {
+		while (true) {
+			try {
+				Thread.sleep(Long.MAX_VALUE);
+			} catch (InterruptedException e) {
+				break;
+			}
+		}
+	}
+	
+	public static void waitSeconds(int seconds) {
+		try {
+			Thread.sleep(seconds*1000);
+		} catch (InterruptedException e) {
+			return;
+		}
+	}
+	
+	public static Properties loadConfig(String fileName) {
+		Properties prop = new Properties();
+		if (fileName == null) {
+			fileName = "solicitacoes.alunos.properties";
+		}
+		try {
+			prop.load(new FileInputStream(fileName));
+		} catch(FileNotFoundException e) {
+			System.err.println("File not found " +e.getMessage());
+			System.exit(1);
+		} catch(IOException e) {
+			System.err.println("IOException "+e.getMessage());
+			System.exit(1);
+		}
+		return prop;
 	}
 
 }
