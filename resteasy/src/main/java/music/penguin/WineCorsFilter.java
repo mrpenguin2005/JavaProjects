6166c35b13974acc098501075db8372e93e0cb38
Alberto Pires <mrpenguin2005@gmail.com>
2015-06-24 19:33:43 -0300

    Cors

---
+++ resteasy/src/main/java/music/penguin/WineCorsFilter.java
@@ -0,0 +1,20 @@
+package music.penguin;
+
+import javax.ws.rs.container.PreMatching;
+import javax.ws.rs.ext.Provider;
+
+import org.jboss.resteasy.plugins.interceptors.CorsFilter;
+
+@Provider
+@PreMatching
+public class WineCorsFilter extends CorsFilter {
+
+	public WineCorsFilter() {
+		super();
+		setAllowedHeaders("Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+		setAllowedMethods("POST, GET, HEAD, PUT, DELETE, OPTIONS");
+		getAllowedOrigins().add("*");
+
+	}
+
+}
