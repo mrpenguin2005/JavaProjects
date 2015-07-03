56aa71b39e9d671ea12faf91a1c0f783fd07f5fa
Alberto Pires <mrpenguin2005@gmail.com>
2015-07-03 19:05:36 -0300

    angular

---
+++ resteasy/WebContent/js/wine.js
@@ -0,0 +1,8 @@
+var app = angular.module('myApp', []);
+
+app.controller('customersCtrl', function($scope, $http) {
+	$http.get("http://localhost:8080/resteasy/ooo/data/wine/user").success(
+			function(response) {
+				$scope.wines = response;
+			});
+});
