a1fa05b13c504f8f956cbbd5d4a94d008b3aede2
Alberto Pires <mrpenguin2005@gmail.com>
2015-07-22 19:00:15 -0300

    ui-router

---
+++ resteasy/WebContent/app/wcontroller.js
@@ -0,0 +1,30 @@
+var app = angular.module('myApp', ['wineServices','ui.router']);
+
+app.controller('wineController',['$scope','Wine', function($scope, Wine) {
+	function winesUser() {
+		Wine.obter($scope.userId).get({}, function(result) {
+			$scope.wines = result;
+		}); 
+	}
+//	Wine.obter($scope.userId).get({}, function(result) {
+//		$scope.wines = result;
+//	});	
+	$scope.search = function() {
+		if ($scope.userId) {
+			winesUser();
+		}
+	}
+}]);
+
+app.config(function($stateProvider, $urlRouterProvider) {
+	//
+	// For any unmatched url, redirect to /state1
+	//$urlRouterProvider.otherwise("/state1");
+	
+	$stateProvider
+    .state('search', {
+      url: "/search",
+      templateUrl: "wsearch.html",
+      controller: 'wineController'
+    });
+});
